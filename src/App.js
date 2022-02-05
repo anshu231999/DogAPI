49b5b66bb152555977a01fb26c87ede36af7fa7f
learner_anshu <anshushandilyagaya@gmail.com>
2022-02-05 23:37:35 +0530

    Save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
-
+import add from './add.jpg';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [post, setPost] = useState([]);
+
+ useEffect(() => {
+   
+    axios.get("https://dog.ceo/api/breeds/list/all").then((response) => {
+        //response.data.message.map((dog,key) => console.log(dog));
+      
+          for(var i in response.data.message)
+          {
+              // console.log(i);
+               if(response.data.message[i].length !==0)
+               {
+                   for(var j of response.data.message[i])
+                   {
+                     // console.log(i+" "+j);
+                      post.push(i+" "+j); 
+                     // setPost(...+(i+" "+j));
+                   }
+               }
+               else
+                {
+                     post.push(i); 
+                   // setPost(...+(i)); 
+                }      
+       }     
+    
+   
+    });           
+  },[]);
+  const listNames = post.map((breed,key)=>              
+  <>       
+     
+  <div className="val"> 
+     {/* console.log(key)*/}
+    <img src={add} alt="Dog" style={{width:"100px", height:"150px", borderRadius:"8px"}}/>  
+  
+       
+      <p>{breed}</p>
+    </div> 
+   
+   </>   
+  );    
+  return (   
+    <div className="App">                                                          
+    <h1>Dog Breed</h1>  
+          
+    <div className="container">    
+     
+          
+         {listNames}  
+           
     </div>
+    </div>
+   
   );
 }
 
