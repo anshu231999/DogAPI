03ef6a7df7469edded6019881a794d0c5aced3d9
learner_anshu <anshushandilyagaya@gmail.com>
2022-02-06 11:15:14 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
 import add from './add.jpg';
+import Add1 from './Add1.jpg';
+import add2 from './add2.jpg';  
 function App() {
   const [post, setPost] = useState([]);
-
+  const imgLink=[add,Add1,add2];
+  const link="https://dog.ceo/api/breeds/list/all";
  useEffect(() => {
-   
-    axios.get("https://dog.ceo/api/breeds/list/all").then((response) => {
+    
+    axios.get(link).then((response) => {
         //response.data.message.map((dog,key) => console.log(dog));
-      
+         const newPost=[];
           for(var i in response.data.message)
           {
               // console.log(i);
@@ -18,26 +21,27 @@ function App() {
                    for(var j of response.data.message[i])
                    {
                      // console.log(i+" "+j);
-                      post.push(i+" "+j); 
+                      newPost.push(i+" "+j); 
                      // setPost(...+(i+" "+j));
                    }
                }
                else
                 {
-                     post.push(i); 
+                     newPost.push(i); 
                    // setPost(...+(i)); 
                 }      
-       }     
-    
+       } 
+       setPost(post=>newPost);    
+     
    
     });           
-  },[]);
-  const listNames = post.map((breed,key)=>              
-  <>       
-     
+  },[]);  
+  const listNames = post.map((breed,key)=>                
+  <>    
+   
   <div className="val"> 
      {/* console.log(key)*/}
-    <img src={add} alt="Dog" style={{width:"100px", height:"150px", borderRadius:"8px"}}/>  
+    <img src={imgLink[key%imgLink.length]} alt="Dog" style={{width:"100px", height:"150px", borderRadius:"8px"}}/>  
   
        
       <p>{breed}</p>
